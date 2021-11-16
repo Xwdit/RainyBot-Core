d8c84af6a3f01c2180a31f2883259039c2625088
Xwdit <xwditfr@gmail.com>
2021-11-16 21:21:51 +0100

    进一步完善细节

--- Core/HttpRequestManager.gd
+++ Core/HttpRequestManager.gd
@@ -24,3 +24,28 @@ func _tick_request_timeout(request_ins:HttpRequestInstance,_timeout:float):
 	if is_instance_valid(request_ins) && request_ins.result == null:
 		request_ins.emit_signal("request_finished")
 		GuiManager.console_print_error("Http请求超时，无法获取到返回结果: "+str(request_ins.request_url))
+
+
+class HttpRequestInstance:
+	extends HTTPRequest
+
+	signal request_finished
+
+	var request_url = null
+	var result = null
+
+	func _ready():
+		connect("request_completed",Callable(self,"_http_request_completed"))
+
+	func _http_request_completed(_result, _response_code, _headers, body):
+		var json = JSON.new()
+		var _error = json.parse(body.get_string_from_utf8())
+		if _error == OK:
+			GuiManager.console_print_success("获取到Http请求的回应: "+str(result))
+			result = json.get_data()
+		else:
+			GuiManager.console_print_error("无法获取到有效的Http请求回应")
+		emit_signal("request_finished")
+		
+	func get_result():
+		return result
