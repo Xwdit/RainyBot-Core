747a1ecb283ec34c393e04e8147c41547273d91f
Xwdit <xwditfr@gmail.com>
2021-12-31 10:47:29 +0100

    实现基本的菜单框架

--- Gui/MainGui.gd
+++ Gui/MainGui.gd
@@ -3,10 +3,9 @@ extends Control
 
 func _ready():
 	DisplayServer.window_set_title("RainyBot")
-	$TabContainer.set_tab_title(0,"RainyBot控制台")
 	Console.print_success("成功加载 RainyBot-Gui | 版本:V2.0-Beta-1 | 作者:Xwdit")
 	RainyBotCore.start()
 
 
 func _process(_delta):
-	$Status.text = "协议后端:Mirai | %s | Bot ID:%s" % ["已连接" if BotAdapter.is_bot_connected() else "未连接", str(BotAdapter.get_bot_id())]
+	$Status.text = str(Engine.get_frames_per_second())+"协议后端:Mirai | %s | Bot ID:%s" % ["已连接" if BotAdapter.is_bot_connected() else "未连接", str(BotAdapter.get_bot_id())]
