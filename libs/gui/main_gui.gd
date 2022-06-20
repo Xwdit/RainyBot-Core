50a73e361754a009d0d84379a46706175d9efda3
Xwdit <xwditfr@gmail.com>
2022-06-20 13:56:45 +0200

    改进版本更新检测流程

--- libs/gui/main_gui.gd
+++ libs/gui/main_gui.gd
@@ -20,25 +20,21 @@ func _process(_delta):
 func check_update()->bool:
 	Console.print_warning("正在检查您的RainyBot是否为最新版本，请稍候...")
 	var err = $HTTPRequest.request("https://api.github.com/repos/Xwdit/RainyBot-Core/releases/latest")
-	if err != OK:
-		Console.print_error("检查更新时出现错误，请检查网络连接是否正常")
-		return false
-	else:
+	if err==OK:
 		var result = await $HTTPRequest.request_completed
-		var body:PackedByteArray = result[3]
-		var json = JSON.new()
-		json.parse(body.get_string_from_utf8())
-		var dic:Dictionary = json.get_data()
-		if !dic.is_empty() and dic.has("tag_name"):
-			var version:String = dic["tag_name"]
-			if RainyBotCore.VERSION.to_lower() != version.to_lower():
-				Console.print_warning("发现RainyBot新版本! 最新版本为: %s, 您的当前版本为: %s"%[version.to_lower(),RainyBotCore.VERSION.to_lower()])
-				Console.print_warning("您可以访问以下网址来获取最新版本: https://github.com/Xwdit/RainyBot-Core/releases")
-				return false
-			else:
-				Console.print_success("版本检查完毕，您的RainyBot已为最新版本！")
-				return true
-		else:
-			Console.print_error("检查更新时出现错误，请检查网络连接是否正常")
-			return false
-			
+		if result[0] == HTTPRequest.RESULT_SUCCESS:
+			var body:PackedByteArray = result[3]
+			var json:JSON = JSON.new()
+			var json_err:int = json.parse(body.get_string_from_utf8())
+			if json_err == OK:
+				var dic:Dictionary = json.get_data()
+				if !dic.is_empty() and dic.has("tag_name"):
+					var version:String = dic["tag_name"]
+					if RainyBotCore.VERSION.to_lower() != version.to_lower():
+						Console.print_warning("发现RainyBot新版本! 最新版本为: %s, 您的当前版本为: %s"%[version.to_lower(),RainyBotCore.VERSION.to_lower()])
+						Console.print_warning("您可以访问以下网址来获取最新版本: https://github.com/Xwdit/RainyBot-Core/releases")
+						return false
+					Console.print_success("版本检查完毕，您的RainyBot已为最新版本！")
+					return true
+	Console.print_error("检查更新时出现错误，请检查网络连接是否正常")
+	return false	
