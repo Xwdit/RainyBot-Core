61e6be8fe7092df0214c95e3aa3147db9b166a4b
Xwdit <xwditfr@gmail.com>
2022-09-19 04:38:36 +0200

    更正api文档链接

--- libs/gui/interfaces/plugin_editor/plugin_editor.gd
+++ libs/gui/interfaces/plugin_editor/plugin_editor.gd
@@ -81,7 +81,7 @@ func _on_SaveReloadButton_button_down()->void:
 
 
 func _on_HelpButton_button_down()->void:
-	OS.shell_open("https://docs.rainybot.dev")
+	OS.shell_open("https://docs.rainybot.dev/api")
 
 
 func _input(event:InputEvent)->void:
