7331d1878c4011aac485aa3abf3a626a377083ab
Xwdit <xwditfr@gmail.com>
2023-01-14 03:59:16 +0100

    更新以适配gif导出模块

--- libs/core/api/CoreAPI/GifImage.gd
+++ libs/core/api/CoreAPI/GifImage.gd
@@ -7,13 +7,8 @@ class_name GifImage
 signal changed()
 
 
-const GIFExporter = preload("res://libs/core/modules/gif_exporter/exporter.gd")
-const MedianCutQuantization = preload("res://libs/core/modules/gif_exporter/quantization/median_cut.gd")
-
-
 var size:Vector2
 var frames:Array[Dictionary]
-var data:PackedByteArray
 var frame_generate_time:int = -1
 
 
@@ -37,7 +32,6 @@ func add_frame(image:Image,delay_time:float)->int:
 		GuiManager.console_print_error("无法将指定图像添加到Gif帧，请检查图像内容是否为空!")
 		return ERR_INVALID_DATA
 	frames.append({"image":image,"delay_time":delay_time})
-	data.resize(0)
 	emit_signal("changed")
 	return OK
 
@@ -54,7 +48,6 @@ func insert_frame(idx:int,image:Image,delay_time:float)->int:
 			GuiManager.console_print_error("无法将指定图像插入到Gif帧，请检查图像内容是否为空!")
 			return ERR_INVALID_DATA
 		frames.insert(idx,{"image":image,"delay_time":delay_time})
-		data.resize(0)
 		if idx == 0:
 			frame_generate_time = -1
 		emit_signal("changed")
@@ -67,7 +60,6 @@ func insert_frame(idx:int,image:Image,delay_time:float)->int:
 func remove_frame(idx:int)->int:
 	if idx <= frames.size()-1:
 		frames.remove_at(idx)
-		data.resize(0)
 		if idx == 0:
 			frame_generate_time = -1
 		emit_signal("changed")
@@ -100,7 +92,6 @@ func set_frame_image(idx:int,image:Image)->int:
 			GuiManager.console_print_error("无法将指定图像设置到Gif帧，请检查图像内容是否为空!")
 			return ERR_INVALID_DATA
 		if frames[idx].image != image:
-			data.resize(0)
 			frames[idx].image = image
 			if idx == 0:
 				frame_generate_time = -1
@@ -117,7 +108,6 @@ func set_frame_delay_time(idx:int,delay_time:float)->int:
 			GuiManager.console_print_error("指定的图像帧延迟时间需要大于0，因此无法将其添加到Gif帧!")
 			return ERR_INVALID_PARAMETER
 		if frames[idx].delay_time != delay_time:
-			data.resize(0)
 			frames[idx].delay_time = delay_time
 			if idx == 0:
 				frame_generate_time = -1
@@ -131,7 +121,6 @@ func set_frame_delay_time(idx:int,delay_time:float)->int:
 func clear_frames()->void:
 	frames.clear()
 	frame_generate_time = -1
-	data.resize(0)
 	emit_signal("changed")
 
 
@@ -146,7 +135,6 @@ func set_size(new_size:Vector2)->int:
 	if size != new_size:
 		size = new_size
 		frame_generate_time = -1
-		data.resize(0)
 		emit_signal("changed")
 	return OK
 
@@ -169,37 +157,25 @@ func save(path:String)->int:
 	if frames.is_empty():
 		GuiManager.console_print_error("此Gif图像实例中不存在任何图像帧，因此无法将其保存为文件！")
 		return ERR_DOES_NOT_EXIST
-	var file:FileAccess = FileAccess.open(path, FileAccess.WRITE)
-	if file:
-		file.store_buffer(await get_data())
-		GuiManager.console_print_success("成功将Gif图像数据储存至文件 %s"% path)
-	else:
-		GuiManager.console_print_error("无法将指定Gif图像数据储存到文件 %s，请检查文件路径或权限是否正确!"% path)
-	return FileAccess.get_open_error()
-
-
-func get_data()->PackedByteArray:
-	if frames.is_empty():
-		GuiManager.console_print_error("此Gif图像实例中不存在任何图像帧，因此无法获取其图像数据！")
-		return PackedByteArray([])
-	if !data.is_empty():
-		return data
 	GuiManager.console_print_warning("正在生成Gif图像数据，请稍候.....")
 	var _start_time:int = Time.get_ticks_msec()
 	var _thread:Thread = Thread.new()
-	var _err:int = _thread.start(_export_data.bind(frames))
+	var _err:int = _thread.start(_export_data.bind(path,frames))
 	if !_err:
 		while _thread.is_alive():
 			await GlobalManager.get_tree().physics_frame
-		data = _thread.wait_to_finish()
+		var _result:bool = _thread.wait_to_finish()
+		if !_result:
+			GuiManager.console_print_error("无法将指定Gif图像数据储存到文件 %s，请检查文件路径或权限是否正确!"% path)
+			return ERR_CANT_CREATE
 		var _end_time:int = Time.get_ticks_msec()
 		var _passed_time:int = _end_time-_start_time
 		frame_generate_time = int(round(float(_passed_time)/float(frames.size())))
-		GuiManager.console_print_success("Gif图像数据生成完毕，正在返回生成的数据.....(总用时: %s秒)"% (float(_passed_time)/1000.0))
-		return data
+		GuiManager.console_print_success("成功将Gif图像数据储存至文件 %s (总用时: %s秒)"% [path,float(_passed_time)/1000.0])
+		return OK
 	else:
-		GuiManager.console_print_error("无法创建用于生成Gif图像数据的线程，请再试一次!")
-		return PackedByteArray([])
+		GuiManager.console_print_error("无法创建用于生成Gif图像的线程，请再试一次!")
+		return ERR_CANT_CREATE
 
 
 func get_generate_time()->float:
@@ -214,15 +190,22 @@ func get_generate_time()->float:
 
 
 func _test_generate_speed()->int:
+	if frames.is_empty():
+		return ERR_CANT_RESOLVE
 	GuiManager.console_print_warning("正在测试Gif图像数据的生成速度，请稍候.....")
 	var _test_frames:Array = [frames[0]]
 	var _start_time:int = Time.get_ticks_msec()
+	var _f_path:String = GlobalManager.cache_path+"gif-image-test-"+Time.get_datetime_string_from_system().replace(":","-")+"-"+str(randi())+".gif"
 	var _thread:Thread = Thread.new()
-	var _err:int = _thread.start(_export_data.bind(_test_frames))
+	var _err:int = _thread.start(_export_data.bind(_f_path,_test_frames))
 	if !_err:
 		while _thread.is_alive():
 			await GlobalManager.get_tree().physics_frame
-		var _data:PackedByteArray = _thread.wait_to_finish()
+		var _result:bool = _thread.wait_to_finish()
+		if !_result:
+			return ERR_CANT_CREATE
+		if FileAccess.file_exists(_f_path):
+			DirAccess.remove_absolute(_f_path)
 		var _end_time:int = Time.get_ticks_msec()
 		frame_generate_time = _end_time-_start_time
 		GuiManager.console_print_success("Gif图像数据生成速度测试完毕！相关数据已保存至该Gif图像实例，以便用于预估生成时长！")
@@ -232,11 +215,25 @@ func _test_generate_speed()->int:
 		return ERR_CANT_CREATE
 
 
-func _export_data(_img_frames:Array)->PackedByteArray:
-	var _exporter:GIFExporter = GIFExporter.new(size.x,size.y)
+func _export_data(_file_path:String,_img_frames:Array)->bool:
+	if _img_frames.is_empty():
+		return false
+	var _exporter:GifExporter = GifExporter.new()
+	var _success:bool = _exporter.begin_export(_file_path,size.x,size.y,int(round(_img_frames[0].delay_time*100)))
+	if !_success:
+		_exporter.end_export()
+		if FileAccess.file_exists(_file_path):
+			DirAccess.remove_absolute(_file_path)
+		return false
 	for _img_dic in _img_frames:
 		var _img:Image = _img_dic.image
 		var _delay:int = int(round(_img_dic.delay_time*100))
 		_img.convert(Image.FORMAT_RGBA8)
-		_exporter.add_frame(_img,_delay,MedianCutQuantization)
-	return _exporter.export_file_data()
+		_success = _exporter.write_frame(_img,Color.WHITE,_delay)
+		if !_success:
+			_exporter.end_export()
+			if FileAccess.file_exists(_file_path):
+				DirAccess.remove_absolute(_file_path)
+			return false
+	_exporter.end_export()
+	return true
--- libs/core/modules/gif_exporter/little_endian.gd
+++
@@ -1,5 +0,0 @@
-extends RefCounted
-
-
-func int_to_2bytes(value:int) -> PackedByteArray:
-	return PackedByteArray([value & 255, (value >> 8) & 255])
